fcfb7fb905c251206780ac5b9d0e7bd0ab62f9de
LueKely <luekely43110@gmail.com>
2024-08-04 10:21:43 +0800

    Build: Changed dependencies

--- src/App.tsx
+++ src/App.tsx
@@ -28,7 +28,7 @@ function App() {
 			<p className="read-the-docs">
 				Click on the Vite and React logos to learn more
 			</p>
-			<Button>This is a shadbutton</Button>
+			<Button>This is a button</Button>
 		</>
 	);
 }
---
+++ src/pages/Root.tsx
@@ -0,0 +1,9 @@
+function Root() {
+	return (
+		<>
+			<p>This is the root</p>
+		</>
+	);
+}
+
+export default Root;
