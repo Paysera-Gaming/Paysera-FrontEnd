189dd0c412101d37f000fb15666b157b349f2728
Jazz Nase <jazzmichaelnase@gmail.com>
2024-08-10 10:01:23 +0100

    FEAT: Added Projects.tsx

--- src/main.tsx
+++ src/main.tsx
@@ -1,8 +1,8 @@
-// src/main.tsx
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import App from './App';
 import YourTeam from './components/AdminDashboard/YourTeam';
+import Projects from './components/AdminDashboard/Projects'; // Import the Projects component
 import './index.css';
 import { createBrowserRouter, RouterProvider } from 'react-router-dom';
 
@@ -14,6 +14,10 @@ const router = createBrowserRouter([
   {
     path: '/yourteam',
     element: <YourTeam />, // Route for YourTeam component
+  },
+  {
+    path: '/projects',
+    element: <Projects />, // Route for Projects component
   },
 ]);
 
