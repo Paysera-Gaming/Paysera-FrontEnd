760289181bbdf7ab9539ce1642f85dc04f70e7e5
LueKely <luekely43110@gmail.com>
2024-08-15 10:07:05 +0800

    added necessary information

--- src/components/TimeInComponent/TimeBar.tsx
+++ src/components/TimeInComponent/TimeBar.tsx
@@ -1,7 +1,11 @@
 export default function Timebar() {
 	return (
 		<header className="border-border border-solid border w-full rounded-md p-5">
-			Card bitch
+			<span>00:00:00</span>
+			<span>Schedule for today</span>
+			<span>Schedule Type</span>
+			<span>time in button</span>
+			<span>lunch time button</span>
 		</header>
 	);
 }
