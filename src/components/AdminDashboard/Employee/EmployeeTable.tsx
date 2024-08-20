048b4a9cc154d633dd893a45ab1b60ccf594436c
Jazz Nase <jazzmichaelnase@gmail.com>
2024-08-20 08:29:44 +0800

    REMOVED: icon in EmployeeTable

--- src/components/AdminDashboard/Employee/EmployeeTable.tsx
+++ src/components/AdminDashboard/Employee/EmployeeTable.tsx
@@ -2,7 +2,6 @@ import React from 'react';
 import { Table, TableBody, TableCell, TableHead, TableHeader, TableRow } from "@/components/ui/table";
 import { Button } from "@/components/ui/button";
 import { Tooltip, TooltipProvider, TooltipTrigger, TooltipContent } from "@/components/ui/tooltip";
-import { UserIcon, ClockIcon, UsersIcon, ShieldCheckIcon } from '@heroicons/react/24/outline';
 import { handleSort } from './EmployeeActions'; 
 
 const EmployeeTable = ({ data, setSelectedEmployee, setIsDialogOpen, handleDeleteEmployee }) => {
@@ -16,16 +15,16 @@ const EmployeeTable = ({ data, setSelectedEmployee, setIsDialogOpen, handleDelet
       <TableHeader>
         <TableRow>
           <TableHead className="text-center border-x cursor-pointer" onClick={() => handleSort('lastName')}>
-            <UserIcon className="w-5 h-5 inline mr-2" /> Full Name
+            Full Name
           </TableHead>
           <TableHead className="text-center border-x cursor-pointer" onClick={() => handleSort('status')}>
-            <ClockIcon className="w-5 h-5 inline mr-2" /> Status
+            Status
           </TableHead>
           <TableHead className="text-center border-x cursor-pointer" onClick={() => handleSort('team')}>
-            <UsersIcon className="w-5 h-5 inline mr-2" /> Team & Role
+            Team & Role
           </TableHead>
           <TableHead className="text-center border-x cursor-pointer" onClick={() => handleSort('type')}>
-            <ShieldCheckIcon className="w-5 h-5 inline mr-2" /> Type
+            Type
           </TableHead>
           <TableHead className="text-center border-x">Actions</TableHead>
         </TableRow>
@@ -38,27 +37,26 @@ const EmployeeTable = ({ data, setSelectedEmployee, setIsDialogOpen, handleDelet
             <TableCell className="text-center border-x">{employee.team} - {employee.role}</TableCell>
             <TableCell className="text-center border-x">{employee.type}</TableCell>
             <TableCell className="text-center border-x space-x-2">
-  <TooltipProvider>
-    <Tooltip>
-      <TooltipTrigger asChild>
-        <Button
-          onClick={() => handleEditEmployee(employee)}
-          className="bg-blue-500 text-white hover:bg-blue-600" // Add hover:bg-blue-600 for hover effect
-        >
-          Edit
-        </Button>
-      </TooltipTrigger>
-      <TooltipContent>Edit Employee</TooltipContent>
-    </Tooltip>
-  </TooltipProvider>
-  <Button
-    onClick={() => handleDeleteEmployee(employee.id)}
-    className="bg-red-500 text-white hover:bg-red-600" // Add hover:bg-red-600 for hover effect
-  >
-    Delete
-  </Button>
-</TableCell>
-
+              <TooltipProvider>
+                <Tooltip>
+                  <TooltipTrigger asChild>
+                    <Button
+                      onClick={() => handleEditEmployee(employee)}
+                      className="bg-blue-500 text-white hover:bg-blue-600"
+                    >
+                      Edit
+                    </Button>
+                  </TooltipTrigger>
+                  <TooltipContent>Edit Employee</TooltipContent>
+                </Tooltip>
+              </TooltipProvider>
+              <Button
+                onClick={() => handleDeleteEmployee(employee.id)}
+                className="bg-red-500 text-white hover:bg-red-600"
+              >
+                Delete
+              </Button>
+            </TableCell>
           </TableRow>
         ))}
       </TableBody>
