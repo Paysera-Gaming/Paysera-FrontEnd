d87f69ad7d9412c7e161a7080ac2d308e91b6722
Jazz Nase <jazzmichaelnase@gmail.com>
2024-08-21 15:09:08 +0800

    mhkh

--- src/pages/AdminPage/AdminPage.tsx
+++ src/pages/AdminPage/AdminPage.tsx
@@ -4,17 +4,16 @@ import { PaginationComponent } from '@/components/AdminDashboard/PaginationCompo
 import SheetComponent from '@/components/AdminDashboard/SheetComponent';
 
 // Import CSS files
-import './AdminDashboard/calendar.css';
-import './AdminDashboard/dialog-sheet.css';
-import './AdminDashboard/general.css';
-import './AdminDashboard/header.css';
-import './AdminDashboard/links-logout.css';
-import './AdminDashboard/main-content.css';
-import './AdminDashboard/profile.css';
-import './AdminDashboard/settings.css';
-import './AdminDashboard/status-situation.css';
-import './AdminDashboard/table.css';
-import './App.css'; // Your main App CSS
+import '../../AdminDashboard/calendar.css';
+import '../../AdminDashboard/dialog-sheet.css';
+import '../../AdminDashboard/general.css';
+import '../../AdminDashboard/header.css';
+import '../../AdminDashboard/links-logout.css';
+import '../../AdminDashboard/main-content.css';
+import '../../AdminDashboard/profile.css';
+import '../../AdminDashboard/settings.css';
+import '../../AdminDashboard/status-situation.css';
+import '../../AdminDashboard/table.css';
 
 function AdminPage() {
 	const [currentTime, setCurrentTime] = useState<string>('');
