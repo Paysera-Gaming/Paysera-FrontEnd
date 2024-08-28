25b6569a63855b201adad2a8e5ea5347c6b51bf9
LueKely <luekely43110@gmail.com>
2024-08-28 16:55:51 +0800

    mini stop upang magbatad ng meslu

---
+++ src/components/TeamLeadComponents/DashboardTeamLead/EmployeeInfoCard.tsx
@@ -0,0 +1,57 @@
+import { Badge } from '@/components/ui/badge';
+import {
+	Card,
+	CardContent,
+	CardDescription,
+	CardFooter,
+	CardHeader,
+	CardTitle,
+} from '@/components/ui/card';
+import { Book, BookUser } from 'lucide-react';
+
+export default function EmployeeInfoCard() {
+	return (
+		<div className="flex flex-col gap-2">
+			<Card className="flex-1 p-0">
+				<CardHeader className="pb-2 flex items-center justify-between flex-row">
+					<CardTitle>Employee Info</CardTitle>
+					<BookUser size={'1.8rem'}></BookUser>
+				</CardHeader>
+				<CardContent>
+					<ul>
+						<li>
+							<b>Name:</b> John Doe
+						</li>
+						<li>
+							<b>Department: </b>Call Center
+						</li>
+						<li>
+							<b>Role:</b> <Badge> Team Lead</Badge>
+						</li>
+						<li>
+							<b> Overtime: </b>
+							<Badge variant={'default'}> Allowed</Badge>
+						</li>
+						<li>
+							<b>Sched type:</b> <Badge>Flexi</Badge>
+						</li>
+						<li>
+							<b>Today's Schedule</b>: 8:00 - 15:00
+						</li>
+					</ul>
+				</CardContent>
+			</Card>
+			<Card className="border-green-600  text-green-600 bg-green-50">
+				<CardHeader className=" pb-0 flex flex-row items-center justify-between">
+					<CardTitle className="">Handbook</CardTitle>
+					<Book></Book>
+				</CardHeader>
+				<CardContent className="">
+					<CardDescription className="text-green-500">
+						Click here to open
+					</CardDescription>
+				</CardContent>
+			</Card>
+		</div>
+	);
+}
