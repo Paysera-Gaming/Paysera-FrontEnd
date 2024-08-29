0231dcd068e2586c8fd69a5461b0a493819e3e23
Jazz Nase <jazzmichaelnase@gmail.com>
2024-08-29 08:58:03 +0800

    REMOVED: some errors in AccountPreferences

--- src/components/AdminDashboard/AccountPreferences/AvatarUpload.tsx
+++ src/components/AdminDashboard/AccountPreferences/AvatarUpload.tsx
@@ -1,4 +1,3 @@
-import React, { useState } from 'react';
 import { Avatar, AvatarImage, AvatarFallback } from '../../ui/avatar';
 
 const AvatarUpload = () => {
--- src/components/AdminDashboard/AccountPreferences/Preferences.tsx
+++ src/components/AdminDashboard/AccountPreferences/Preferences.tsx
@@ -1,4 +1,3 @@
-import React from 'react';
 import { Switch } from '../../ui/switch';
 
 const Preferences = ({ darkMode, setDarkMode }) => {
