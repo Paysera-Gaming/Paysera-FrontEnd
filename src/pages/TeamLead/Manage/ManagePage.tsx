9a106a31f6cf9cab1d2fdce408f6cdade8d76e4d
LueKely <luekely43110@gmail.com>
2024-08-30 11:24:12 +0800

    whoopsies

--- src/pages/TeamLead/Manage/ManagePage.tsx
+++ src/pages/TeamLead/Manage/ManagePage.tsx
@@ -2,6 +2,9 @@ export default function ManagePage() {
 	return (
 		<div className="grid grid-cols-3 gap-3  w-full h-full border-border border-solid border p-5 rounded-md">
 			this is the managepage
+			{/* search bar sa table with a add employee of the side then after adding empoyee may form na lalagay anong role nya  */}
+			{/* and table of employeees */}
+			{/*  */}
 		</div>
 	);
 }
