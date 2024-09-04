a6eb3e9fd64844e874c776aad9e992fd44b855f6
Jazz Nase <jazzmichaelnase@gmail.com>
2024-09-04 16:42:28 +0800

    sad life

--- src/components/AdminDashboard/CalendarComponent/utils.tsx
+++
@@ -1,6 +0,0 @@
-import { format } from "date-fns";
-
-export const formatDate = (date: Date | number, formatString: string) => {
-  return format(date, formatString);
-};
-    
--- src/components/AdminDashboard/ManageTeams/validation.ts
+++
@@ -1,9 +0,0 @@
-export const isValidName = (name: string) => {
-  const regex = /^[a-zA-Z]+\s*,\s*[a-zA-Z]+\s*(?:[a-zA-Z]+)?$/;
-  return regex.test(name);
-};
-
-export const isValidEmail = (email: string) => {
-  const regex = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
-  return regex.test(email);
-};
