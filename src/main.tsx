bb06b4dbeb5a914d6d41d8042393b94bf4d1d762
LueKely <luekely43110@gmail.com>
2024-09-14 23:59:52 +0800

    Bagong pilipinas bagong mukha

--- src/main.tsx
+++ src/main.tsx
@@ -1,8 +1,5 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
-
-const queryClient = new QueryClient();
 // login
 import LoginPage from './pages/Login/LoginPage';
 // employee pages
@@ -28,9 +25,9 @@ import './index.css';
 import { ThemeProvider } from '@/components/ThemeProvider/ThemeProvider';
 
 import {
-    createBrowserRouter,
-    RouterProvider,
-    redirect,
+	createBrowserRouter,
+	RouterProvider,
+	redirect,
 } from 'react-router-dom';
 import ProtectedRoute from './lib/AccessLevelUtils';
 
@@ -126,13 +123,12 @@ const router = createBrowserRouter([
 				),
 			},
 
-            // Add more child routes if needed
-        ],
-    },
+			// Add more child routes if needed
+		],
+	},
 ]);
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
-
 	<React.StrictMode>
 		<QueryClientProvider client={queryClient}>
 			<ThemeProvider>
@@ -141,4 +137,3 @@ ReactDOM.createRoot(document.getElementById('root')!).render(
 		</QueryClientProvider>
 	</React.StrictMode>
 );
-
