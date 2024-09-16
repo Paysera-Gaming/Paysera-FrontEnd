00f9b793bede4430a7b709f7773582f2af1509d3
LueKely <luekely43110@gmail.com>
2024-09-16 09:30:14 +0800

    please work

--- src/api/EmployeeAPI.ts
+++ src/api/EmployeeAPI.ts
@@ -30,7 +30,7 @@ export async function addEmployeeInDepartment(
 ): Promise<TAddEmployee> {
 	const response: AxiosResponse<TEmployee> = await axiosInstance.put(
 		`/api/department/${departmentId}/employee`,
-		{ userName: employee.username, role: employee.role }
+		{ username: employee.username, role: employee.role }
 	);
 
 	return response.data;
