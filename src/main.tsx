c662ee29b77990842e5b0cc13badd306d490324d
LueKely <luekely43110@gmail.com>
2024-09-17 21:47:25 +0800

    fixing main page

--- src/main.tsx
+++ src/main.tsx
@@ -1,19 +1,24 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-// login
+import {
+	createBrowserRouter,
+	RouterProvider,
+	redirect,
+} from 'react-router-dom';
+import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
+import { Toaster } from '@/components/ui/sonner';
+import { ThemeProvider } from '@/components/ThemeProvider/ThemeProvider';
+import ProtectedRoute from './lib/AccessLevelUtils';
+
 import LoginPage from './pages/Login/LoginPage';
-// employee pages
 import EmployeePage from './pages/Employee/EmployeePage';
 import EmployeeDashboardPage from './pages/Employee/Dashboard/DashboardPage';
 import RequestPage from './pages/Employee/Request/RequestPage';
-// Teamlead pages
 import TeamLeadPage from './pages/TeamLead/TeamLeadPage';
 import TeamLeadDashboardPage from './pages/TeamLead/Dashboard/DashboardPage';
 import SchedulePage from './pages/TeamLead/Schedule/SchedulePage';
 import AttendancePage from './pages/TeamLead/Attendance/AttendancePage';
 import ManagePage from './pages/TeamLead/Manage/ManagePage';
-
-// superadmin
 import SuperAdminPage from './pages/SuperAdmin/SuperAdminPage';
 import SuperAdminDashboardPage from './pages/SuperAdmin/Dashboard/DashboardPage';
 import AttendanceDashboard from './pages/SuperAdmin/Attendance/AttendanceDashboard';
@@ -21,22 +26,11 @@ import SuperAdminEmployeeDashboard from './pages/SuperAdmin/Employee/SuperAdminE
 import SuperAdminDepartmentDashboard from './pages/SuperAdmin/Department/SuperAdminDepartmentDashboard';
 
 import './index.css';
-// Theme provider
-import { ThemeProvider } from '@/components/ThemeProvider/ThemeProvider';
-
-import {
-	createBrowserRouter,
-	RouterProvider,
-	redirect,
-} from 'react-router-dom';
-import ProtectedRoute from './lib/AccessLevelUtils';
-
-import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
-import { Toaster } from '@/components/ui/sonner';
 
 // Create a client
 const queryClient = new QueryClient();
 queryClient.setDefaultOptions({});
+
 const router = createBrowserRouter([
 	{
 		path: '/',
@@ -45,87 +39,47 @@ const router = createBrowserRouter([
 	},
 	{
 		path: '/login',
-		element: <LoginPage></LoginPage>,
+		element: <LoginPage />,
 	},
 	{
 		path: '/employee',
 		element: (
-			<ProtectedRoute
-				page={<EmployeePage></EmployeePage>}
-				requiredLevel="EMPLOYEE"
-			></ProtectedRoute>
+			<ProtectedRoute page={<EmployeePage />} requiredLevel="EMPLOYEE" />
 		),
 		children: [
-			{
-				index: true,
-				loader: () => redirect('/employee/dashboard'),
-			},
-			{
-				path: 'dashboard',
-				element: <EmployeeDashboardPage></EmployeeDashboardPage>,
-			},
-			{ path: 'request', element: <RequestPage></RequestPage> },
+			{ index: true, loader: async () => redirect('/employee/dashboard') },
+			{ path: 'dashboard', element: <EmployeeDashboardPage /> },
+			{ path: 'request', element: <RequestPage /> },
 		],
 	},
 	{
 		path: '/teamlead',
 		element: (
-			<ProtectedRoute
-				page={<TeamLeadPage></TeamLeadPage>}
-				requiredLevel="TEAM_LEADER"
-			></ProtectedRoute>
+			<ProtectedRoute page={<TeamLeadPage />} requiredLevel="TEAM_LEADER" />
 		),
 		children: [
-			{
-				index: true,
-				loader: () => redirect('/teamlead/dashboard'),
-			},
-			{
-				path: 'dashboard',
-				element: <TeamLeadDashboardPage></TeamLeadDashboardPage>,
-			},
-			{ path: 'manage', element: <ManagePage></ManagePage> },
-			{ path: 'schedule', element: <SchedulePage></SchedulePage> },
-			{ path: 'attendance', element: <AttendancePage></AttendancePage> },
+			{ index: true, loader: async () => redirect('/teamlead/dashboard') },
+			{ path: 'dashboard', element: <TeamLeadDashboardPage /> },
+			{ path: 'manage', element: <ManagePage /> },
+			{ path: 'schedule', element: <SchedulePage /> },
+			{ path: 'attendance', element: <AttendancePage /> },
 		],
 	},
-
 	{
 		path: '/superadmin',
-		element: (
-			<ProtectedRoute
-				page={<SuperAdminPage></SuperAdminPage>}
-				requiredLevel="ADMIN"
-			></ProtectedRoute>
-		),
+		element: <ProtectedRoute page={<SuperAdminPage />} requiredLevel="ADMIN" />,
 		children: [
-			{
-				index: true,
-				loader: () => redirect('/superadmin/dashboard'),
-			},
-			{
-				path: 'dashboard',
-				element: <SuperAdminDashboardPage />,
-				// SuperAdmin Dashboard
-			},
-			{ path: 'schedule', element: <SchedulePage></SchedulePage> },
-			{
-				path: 'attendance',
-				element: <AttendanceDashboard></AttendanceDashboard>,
-			},
-			{
-				path: 'employee',
-				element: <SuperAdminEmployeeDashboard></SuperAdminEmployeeDashboard>,
-			},
-			{
-				path: 'departments',
-				element: (
-					<SuperAdminDepartmentDashboard></SuperAdminDepartmentDashboard>
-				),
-			},
-
-			// Add more child routes if needed
+			{ index: true, loader: async () => redirect('/superadmin/dashboard') },
+			{ path: 'dashboard', element: <SuperAdminDashboardPage /> },
+			{ path: 'schedule', element: <SchedulePage /> },
+			{ path: 'attendance', element: <AttendanceDashboard /> },
+			{ path: 'employee', element: <SuperAdminEmployeeDashboard /> },
+			{ path: 'departments', element: <SuperAdminDepartmentDashboard /> },
 		],
+	},
+	{
+		path: '*',
+		loader: () => redirect('/login'),
 	},
 ]);
 
@@ -145,7 +99,7 @@ ReactDOM.createRoot(document.getElementById('root')!).render(
 							info: 'bg-blue-400',
 						},
 					}}
-				></Toaster>
+				/>
 			</ThemeProvider>
 		</QueryClientProvider>
 	</React.StrictMode>
