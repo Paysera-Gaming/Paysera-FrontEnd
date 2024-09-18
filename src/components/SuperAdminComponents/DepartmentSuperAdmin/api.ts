b8e8078087e2c4eb21993854f4d6d7f1c508a89c
Jazz Nase <jazzmichaelnase@gmail.com>
2024-09-18 17:21:26 +0800

    THANKS LUE HEHEH: AXIOS TO AXIOS INSTANCE

--- src/components/SuperAdminComponents/DepartmentSuperAdmin/api.ts
+++ src/components/SuperAdminComponents/DepartmentSuperAdmin/api.ts
@@ -1,4 +1,4 @@
-import axios from 'axios';
+import { axiosInstance } from '@/api';
 
 export interface Employee {
   id: number;
@@ -24,30 +24,30 @@ export interface Department {
 }
 
 export const fetchDepartments = async (): Promise<Department[]> => {
-  const response = await axios.get(`${import.meta.env.VITE_BASE_API}/api/department`);
+  const response = await axiosInstance.get("/api/department");
   return response.data;
 };
 
 export const fetchTeamLeaders = async (): Promise<Leader[]> => {
-  const response = await axios.get(`${import.meta.env.VITE_BASE_API}/api/employee/team-leader`);
+  const response = await axiosInstance.get("/api/employee/team-leader");
   return response.data;
 };
 
 export const addDepartment = async (newDepartment: { name: string; leaderId: number }) => {
-  const response = await axios.post(`${import.meta.env.VITE_BASE_API}/api/department`, newDepartment);
+  const response = await axiosInstance.post("/api/department", newDepartment);
   return response.data;
 };
 
 export const updateDepartment = async (updatedDepartment: { id: number; name: string }) => {
-  const response = await axios.put(`${import.meta.env.VITE_BASE_API}/api/department/${updatedDepartment.id}`, { name: updatedDepartment.name });
+  const response = await axiosInstance.put(`/api/department/${updatedDepartment.id}`, { name: updatedDepartment.name });
   return response.data;
 };
 
 export const updateDepartmentLeader = async (updatedLeader: { id: number; leaderId: number }) => {
-  const response = await axios.put(`${import.meta.env.VITE_BASE_API}/api/department/${updatedLeader.id}/leader`, { leaderId: updatedLeader.leaderId });
+  const response = await axiosInstance.put(`/api/department/${updatedLeader.id}/leader`, { leaderId: updatedLeader.leaderId });
   return response.data;
 };
 
 export const deleteDepartment = async (id: number) => {
-  await axios.delete(`${import.meta.env.VITE_BASE_API}/api/department/${id}`);
+  await axiosInstance.delete(`/api/department/${id}`);
 };
