c39055e91dc2e84258d2a2a74b10e271b7f6c5df
LueKely <luekely43110@gmail.com>
2024-09-19 12:31:21 +0800

    added full w

--- src/pages/Employee/Dashboard/DashboardPage.tsx
+++ src/pages/Employee/Dashboard/DashboardPage.tsx
@@ -4,7 +4,7 @@ import RecentActivitiesCard from '@/components/TeamLeadComponents/DashboardTeamL
 
 export default function Dashboard() {
 	return (
-		<div className=" h-full border-border border-solid border p-5 rounded-md">
+		<div className=" h-full w-full border-border border-solid border p-5 rounded-md">
 			<h2 className="col-span-3 scroll-m-20  text-3xl mb-2 font-semibold tracking-tight first:mt-0">
 				Dashboard
 			</h2>
