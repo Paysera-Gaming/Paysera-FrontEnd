07f3596d13f56406256befdbf0ed20b36b290867
Jazz Nase <jazzmichaelnase@gmail.com>
2024-12-03 11:07:22 +0800

    RENOVED: lunch time in and lunch time out

--- src/components/SuperAdminComponents/AttendanceSuperAdmin/exportToCSV.ts
+++ src/components/SuperAdminComponents/AttendanceSuperAdmin/exportToCSV.ts
@@ -3,28 +3,40 @@ import { Attendance } from './types';
 import { formatDate, formatTime, calculateWorkTimeTotal } from './utils';
 import { format } from 'date-fns';
 
+type FormattedData = {
+  'First Name': string;
+  'Last Name': string;
+  'Date': string;
+  'Status': string;
+  'Schedule Type': string;
+  'Time In': string;
+  'Time Out': string;
+  'Lunch Time Total': string | number;
+  'Work Time Total': string | number;
+  'Overtime Total': string | number;
+  'Total Time': string | number;
+};
+
 export const exportToCSV = (data: Attendance[], dateRange: { from: Date; to: Date } | undefined) => {
   const csvData = data.map((attendance) => {
     const workTimeTotal = attendance.timeIn && attendance.timeOut ? calculateWorkTimeTotal(attendance.timeIn, attendance.timeOut, attendance.lunchTimeTotal) : '';
 
-    const formattedData: { [key: string]: any } = {
+    const formattedData: FormattedData = {
       'First Name': attendance.employee.firstName,
       'Last Name': attendance.employee.lastName,
       'Date': formatDate(attendance.date),
       'Status': attendance.status,
       'Schedule Type': attendance.scheduleType,
       'Time In': formatTime(attendance.timeIn),
-      'Lunch Time In': formatTime(attendance.lunchTimeIn),
-      'Lunch Time Out': formatTime(attendance.lunchTimeOut),
       'Time Out': formatTime(attendance.timeOut),
-      'Lunch Time Total': attendance.lunchTimeTotal,
+      'Lunch Time Total': attendance.lunchTimeTotal ?? '',
       'Work Time Total': attendance.timeHoursWorked === 0 ? '' : workTimeTotal,
-      'Overtime Total': attendance.overTimeTotal,
-      'Total Time': attendance.timeTotal,
+      'Overtime Total': attendance.overTimeTotal ?? '',
+      'Total Time': attendance.timeTotal ?? '',
     };
 
     // Replace default values and null/undefined with empty string
-    Object.keys(formattedData).forEach(key => {
+    (Object.keys(formattedData) as (keyof FormattedData)[]).forEach(key => {
       if (formattedData[key] === null || formattedData[key] === undefined || formattedData[key] === '7:30 AM' || formattedData[key] === '07:30 AM' || formattedData[key] === -479597.66) {
         formattedData[key] = '';
       }
