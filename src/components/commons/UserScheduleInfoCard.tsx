519ed6dbfd97c6b6a826d9929487138a498e421b
Lue Kely <luekely43110@gmail.com>
2025-01-09 11:37:26 +0800

    initialized new features

---
+++ src/components/commons/UserScheduleInfoCard.tsx
@@ -0,0 +1,9 @@
+export default function HolidayList(){
+    return <section>
+        <h2> Upcoming Holidays</h2>
+        <ul>
+            <li><p> 01/12 - International thing</p></li>
+            <li><p> 01/12 - International thing</p></li>
+        </ul>
+    </section>
+}
