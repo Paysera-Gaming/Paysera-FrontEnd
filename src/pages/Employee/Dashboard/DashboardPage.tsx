7ab5089b1ba28adb795715c2606d6ca2a051aeb0
Lue Kely <luekely43110@gmail.com>
2025-01-19 12:35:42 +0800

    added the dashboard

--- src/pages/Employee/Dashboard/DashboardPage.tsx
+++ src/pages/Employee/Dashboard/DashboardPage.tsx
@@ -7,17 +7,15 @@ import RecentActivitiesCard from '@/components/TeamLeadComponents/DashboardTeamL
 
 export default function Dashboard() {
 	return (
-		<div className=" h-full w-full border-border border-solid border p-5 rounded-md">
-			<div className=" h-[750px] w-full  grid grid-flow-col grid-rows-6  gap-3 ">
-				<UserInfoCard className="row-span-2"></UserInfoCard>
-				<UserScheduleInfoCard className="col-span-1 row-span-3"></UserScheduleInfoCard>
-				<UserManualCard></UserManualCard>
-				{/*  */}
-				<NoticeBoardCard></NoticeBoardCard>
-				<RecentActivitiesCard></RecentActivitiesCard>
-				{/*  */}
-				<HolidayList></HolidayList>
-			</div>
+		<div className=" w-full h-full border-border border-solid border p-5 rounded-md grid grid-flow-col grid-rows-6 gap-3">
+			<UserInfoCard className="row-span-2"></UserInfoCard>
+			<UserScheduleInfoCard className="col-span-1 row-span-3"></UserScheduleInfoCard>
+			<UserManualCard></UserManualCard>
+			{/*  */}
+			<NoticeBoardCard></NoticeBoardCard>
+			<RecentActivitiesCard></RecentActivitiesCard>
+			{/*  */}
+			<HolidayList></HolidayList>
 		</div>
 	);
 }
