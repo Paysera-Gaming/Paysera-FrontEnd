f421b977a55db39be0913d727fcb36cb1e533628
Lue Kely <luekely43110@gmail.com>
2025-01-21 14:32:14 +0800

    adjusting alert

--- src/components/commons/alertTimeup.tsx
+++ src/components/commons/alertTimeup.tsx
@@ -14,7 +14,6 @@ const changeFavicon = (url: string) => {
 export default function alertTimeup() {
 	document.title = 'Your Time Is Up!';
 	changeFavicon('/Alert.svg');
-
 	toast.warning('Your Time Is Up Please Proceed to your overtime or clockout', {
 		duration: Infinity,
 		onDismiss: () => {
@@ -22,25 +21,4 @@ export default function alertTimeup() {
 			changeFavicon('/PayseraIcon.svg');
 		},
 	});
-
-	// useEffect(() => {
-	// 	const checkTime = () => {
-	// 		const now = new Date();
-	// 		const currentHour = now.getHours();
-	// 		const currentMinute = now.getMinutes();
-
-	// 		// Check if it's 12:00 PM
-	// 		if (userStoreEndTime && currentMinute === 0) {
-	// 			console.log('Time’s up! It’s 12:00 PM!');
-	// 			document.title = 'Your Time Is Up!';
-	// 			toast.warning('Your Time Is Up', { duration: Infinity });
-	// 		}
-	// 	};
-
-	// 	// Set an interval to check the time every second
-	// 	const interval = setInterval(checkTime, 1000);
-
-	// 	// Cleanup
-	// 	return () => clearInterval(interval);
-	// }, []);
 }
