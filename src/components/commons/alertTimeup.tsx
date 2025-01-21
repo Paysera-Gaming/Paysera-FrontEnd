5e0ede965ed48293f4462c76f6283c26fc7e0cd5
Lue Kely <luekely43110@gmail.com>
2025-01-21 14:59:05 +0800

    Added alert notifs, the logic remains

--- src/components/commons/alertTimeup.tsx
+++ src/components/commons/alertTimeup.tsx
@@ -14,6 +14,14 @@ const changeFavicon = (url: string) => {
 export default function alertTimeup() {
 	document.title = 'Your Time Is Up!';
 	changeFavicon('/Alert.svg');
+
+	if (Notification.permission === 'granted') {
+		new Notification('(Paysera Attendace) Your Time Is Up!', {
+			body: 'Either clock out or begin your overtime',
+			badge: '/Alert.svg',
+		});
+	}
+
 	toast.warning('Your Time Is Up Please Proceed to your overtime or clockout', {
 		duration: Infinity,
 		onDismiss: () => {
