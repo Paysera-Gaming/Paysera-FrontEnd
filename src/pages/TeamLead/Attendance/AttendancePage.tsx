86fb4c66ddca072b314ae4409a5d8dc68f37d68d
Lue Kely <luekely43110@gmail.com>
2025-01-26 12:29:53 +0800

    Init Personal Schedule Page

--- src/pages/TeamLead/Attendance/AttendancePage.tsx
+++ src/pages/TeamLead/Attendance/AttendancePage.tsx
@@ -47,7 +47,7 @@ export default function AttendancePage() {
 	return (
 		<div className=" min-h-0 min-w-0 w-full h-full border-border border-solid border p-5 rounded-md">
 			<h2 className="scroll-m-20  text-3xl font-semibold tracking-tight first:mt-0 mb-5">
-				Attendance
+				Department Attendance History
 			</h2>
 
 			{/* table */}
---
+++ src/pages/TeamLead/Personal_Schedule/PersonalSchedulePage.tsx
@@ -0,0 +1,3 @@
+export default function PersonalSchedulePage() {
+	return <div> Personal Schedule Page</div>;
+}
