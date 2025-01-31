311feacc2894a2e8dfd6bef1d46a9794e393dc16
LueKely <luekely43110@gmail.com>
2025-01-31 16:04:13 +0800

    added to render the current month and the next month's holiday only

--- src/components/commons/HolidayList.tsx
+++ src/components/commons/HolidayList.tsx
@@ -1,9 +1,10 @@
 import { useQuery } from '@tanstack/react-query';
 import { Card, CardContent, CardHeader, CardTitle } from '../ui/card';
-import { getHolidays } from '@/api/CommonsAPI';
+import { getHolidays, IHoliday } from '@/api/CommonsAPI';
 import { Skeleton } from '../ui/skeleton';
 import { ScrollArea } from '../ui/scroll-area';
-
+import { addMonths } from 'date-fns';
+import { Badge } from '../ui/badge';
 function addZero(digit: number) {
 	if (digit < 10 && digit > 0) {
 		return '0' + digit;
@@ -15,13 +16,33 @@ function addZero(digit: number) {
 function getMonthNumberFromName(monthName: string) {
 	return addZero(new Date(`${monthName} 1, 2022`).getMonth() + 1);
 }
+
+// upcoming done ongoing
+
+function renderList(holidays: IHoliday[], month: number) {
+	const filteredList = holidays.filter((holiday) => {
+		if (getMonthNumberFromName(holiday.month) == month) {
+			return holiday;
+		}
+	});
+
+	const list = filteredList.reverse().map((day, index) => (
+		<li key={index}>
+			{addZero(day.day)} - {day.name}
+		</li>
+	));
+
+	return list;
+}
+
 export default function HolidayList() {
 	// fetch the holidays here
 	const { data, isError, isLoading, isSuccess } = useQuery({
 		queryKey: ['holidayQuery'],
 		queryFn: getHolidays,
 	});
-
+	const date = new Date();
+	const nextMonth = addMonths(date, 1);
 	if (isLoading) {
 		return <Skeleton className="row-span-6"></Skeleton>;
 	}
@@ -40,12 +61,11 @@ export default function HolidayList() {
 		);
 	}
 	if (isSuccess) {
-		const list = data.reverse().map((day, index) => (
-			<li key={index}>
-				{getMonthNumberFromName(day.month.toLowerCase())}/{addZero(day.day)} -{' '}
-				{day.name}
-			</li>
-		));
+		//  list all dates
+		// sort dates
+		// filter dates that are relevant only to the current month and day
+		const currentMonthList = renderList(data, new Date().getMonth() + 1);
+		const nextMonthList = renderList(data, new Date().getMonth() + 2);
 
 		// const list = <>LE LIST</>;
 
@@ -53,13 +73,21 @@ export default function HolidayList() {
 			<Card className="p-2 2xl:p-5 row-span-6">
 				<CardHeader className="p-0">
 					<CardTitle className="text-base 2xl:text-lg">
-						Upcoming Holidays
+						Upcoming Holidays{' '}
+						{`(${date.toLocaleString('default', { month: 'long' })})`}
 					</CardTitle>
 				</CardHeader>
 				<CardContent className="p-0 pt-1">
 					<ScrollArea className="h-[350px] 2xl:h-[600px]">
-						<ul className="flex flex-col-reverse list-disc list-inside text-sm 2xl:text-base">
-							{list}
+						<ul className="flex flex-col list-disc list-inside text-sm 2xl:text-base">
+							{currentMonthList}
+
+							<h3 className="text-base 2xl:text-lg font-semibold scroll-m-20 tracking-tight py-1">
+								Next Month{' '}
+								{`(${nextMonth.toLocaleString('default', { month: 'long' })})`}
+							</h3>
+
+							{nextMonthList}
 						</ul>
 					</ScrollArea>
 				</CardContent>
