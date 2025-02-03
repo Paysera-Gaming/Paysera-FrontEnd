97291a622ca0445fd3f3a07d9b1dd8b167a862bf
LueKely <luekely43110@gmail.com>
2025-02-03 12:08:53 +0800

    Added types for the login api and edditted user sched info

--- src/components/commons/UserScheduleInfoCard.tsx
+++ src/components/commons/UserScheduleInfoCard.tsx
@@ -1,13 +1,55 @@
+import { TUserInfo } from '@/api/LoginAPI';
+import { TPersonalSchedule } from '@/api/PersonalScheduleAPI';
 import { TDepartmentSchedules } from '@/api/ScheduleAPI';
 import { Card, CardContent, CardHeader, CardTitle } from '@/components/ui/card';
 import { useUserStore } from '@/stores/userStore';
 import { format } from 'date-fns';
 import { Calendar } from 'lucide-react';
 import { useEffect, useState } from 'react';
+import { Badge } from '../ui/badge';
 
 function dateToHours(date: string) {
 	const translatedDate = new Date(date);
 	return format(translatedDate, 'HH:mm');
+}
+
+function PersonalScheduleList({
+	personalSchedule,
+}: {
+	personalSchedule: TPersonalSchedule | undefined;
+}) {
+	const lookUpDay = {
+		MONDAY: 'Mon',
+		TUESDAY: 'Tues',
+		WEDNESDAY: 'Wed',
+		THURSDAY: 'Thurs',
+		FRIDAY: 'Fri',
+		SATURDAY: 'Sat',
+		SUNDAY: 'Sun',
+	};
+
+	if (personalSchedule)
+		return (
+			<>
+				<h3 className="scroll-m-20 text-base font-semibold tracking-tight">
+					Personal Schedule
+				</h3>
+				<li>
+					Schedule Info:{' '}
+					{dateToHours(personalSchedule.Schedule.startTime) +
+						'-' +
+						dateToHours(personalSchedule.Schedule.endTime)}{' '}
+				</li>
+				<li>Schedule Type: {personalSchedule.Schedule.scheduleType}</li>
+				<li>
+					Availability:{' '}
+					{personalSchedule.day.map((days) => (
+						<Badge className="mx-[0.05rem]">{lookUpDay[days]}</Badge>
+					))}
+				</li>
+			</>
+		);
+	else return <li> Personal Schedule: None </li>;
 }
 
 function ScheduleList({
@@ -17,7 +59,7 @@ function ScheduleList({
 }) {
 	if (schedule?.Schedule != undefined) {
 		return (
-			<ul className="text-sm 2xl:text-base w-full">
+			<>
 				<li>Schedule Type: {schedule.Schedule.scheduleType}</li>
 				<li>
 					Schedule Info:{' '}
@@ -25,11 +67,7 @@ function ScheduleList({
 						'-' +
 						dateToHours(schedule.Schedule.endTime)}
 				</li>
-				<li>
-					Personal Schedule Info: SOON
-					{/* {schedule.Schedule.startTime + ' - ' + schedule.Schedule.endTime} */}
-				</li>
-			</ul>
+			</>
 		);
 	} else {
 		return <div>Currently No Schedule Assigned</div>;
@@ -41,25 +79,51 @@ export default function UserScheduleInfoCard({
 }: {
 	className: string;
 }) {
-	const [getUserInfo, setUserInfo] = useState<TDepartmentSchedules | undefined>(
-		undefined
-	);
+	const [getUserDepartmentSchedule, setUserDepartmentSchedule] = useState<
+		TDepartmentSchedules | undefined
+	>(undefined);
+
+	const [getUserPersonalSchedule, setUserPersonalSchedule] = useState<
+		TPersonalSchedule | undefined
+	>(undefined);
+
 	useEffect(() => {
-		if (useUserStore.getState()?.user?.schedule) {
-			setUserInfo(useUserStore.getState()?.user?.schedule);
+		if (useUserStore.getState()?.user) {
+			setUserDepartmentSchedule(
+				useUserStore.getState()?.user?.departmentSchedule
+			);
 		} else {
-			setUserInfo(undefined);
+			setUserDepartmentSchedule(undefined);
 		}
-	}, [getUserInfo]);
+	}, [getUserDepartmentSchedule]);
+
+	useEffect(() => {
+		if (useUserStore.getState()?.user) {
+			console.log('TITE');
+
+			console.log(useUserStore.getState()?.user?.personalSchedule);
+
+			setUserPersonalSchedule(useUserStore.getState()?.user?.personalSchedule);
+		} else {
+			setUserPersonalSchedule(undefined);
+		}
+	}, [getUserPersonalSchedule]);
 
 	return (
 		<Card className={className + 'p-2 pt-0 2xl:p-5'}>
 			<CardHeader className="flex flex-row p-0 items-end justify-between w-full ">
-				<CardTitle className="text-base 2xl:text-lg">Schedule Info</CardTitle>
+				<CardTitle className="text-base 2xl:text-lg">
+					Department Schedule
+				</CardTitle>
 				<Calendar className="2xl:w-[1.25rem] 2xl:h-[1.25rem] h-[1.5rem] w-[1.5rem]"></Calendar>
 			</CardHeader>
 			<CardContent className="p-0">
-				<ScheduleList schedule={getUserInfo}></ScheduleList>
+				<ul className="text-sm 2xl:text-base w-full">
+					<ScheduleList schedule={getUserDepartmentSchedule}></ScheduleList>
+					<PersonalScheduleList
+						personalSchedule={getUserPersonalSchedule}
+					></PersonalScheduleList>
+				</ul>
 			</CardContent>
 		</Card>
 	);
