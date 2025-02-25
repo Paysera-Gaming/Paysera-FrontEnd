941b4dc9a06fbd0380670ac55f51c2e15e4ce4be
Lue Kely <luekely43110@gmail.com>
2025-02-25 14:27:11 +0800

    finished

--- src/pages/Employee/EmployeePage.tsx
+++ src/pages/Employee/EmployeePage.tsx
@@ -5,10 +5,10 @@ import EmployeeNavigation from '@/components/EmployeeComponents/EmployeeNavigati
 
 export default function EmployeePage() {
 	return (
-		<div className="h-[100svh] w-[100svw] flex flex-1 items-center justify-start p-5 gap-5">
+		<div className="h-[100svh] w-[100svw] min-h-0 min-w-0 flex items-center justify-start p-5 gap-10 relative">
 			<EmployeeNavigation></EmployeeNavigation>
 			{/* delete this h full and w full if something krazy happens */}
-			<main className="flex h-full items-center justify-start flex-col flex-1 gap-y-3">
+			<main className="ml-[calc(4rem_+_1.25rem)] flex w-full h-full flex-1 items-center justify-start flex-col gap-y-3">
 				<Timebar></Timebar>
 				<Outlet></Outlet>
 			</main>
