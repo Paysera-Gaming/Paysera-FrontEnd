4cfeae5d23a5a2acfe0b28790ac8ba660cc9aee9
LueKely <luekely43110@gmail.com>
2025-02-28 13:51:16 +0800

    Closes #139 bug (#141)
    * this will be a temporary fix for now until #109 is solved
    
    * fixed for now

--- src/api/index.ts
+++ src/api/index.ts
@@ -1,7 +1,20 @@
 import axios, { AxiosError } from 'axios';
 import { toast } from 'sonner';
 import { redirect } from 'react-router-dom';
+import { useUserStore } from '@/stores/userStore';
 const url = import.meta.env.VITE_BASE_API;
+
+function clearEverything() {
+	window.location.href = '/';
+	useUserStore.getState().clearUser();
+	localStorage.clear();
+	document.cookie.split(';').forEach((c) => {
+		c = c.trim();
+		document.cookie =
+			c.substring(0, c.indexOf('=')) +
+			'=;expires=Thu, 01 Jan 1970 00:00:00 UTC;path=/';
+	});
+}
 
 export const axiosInstance = axios.create({
 	baseURL: url,
@@ -10,7 +23,6 @@ export const axiosInstance = axios.create({
 
 type errorMessage = { message: string };
 
-// Add a response interceptor
 axiosInstance.interceptors.response.use(
 	function (response) {
 		// Any status code that lie within the range of 2xx cause this function to trigger
@@ -22,8 +34,24 @@ axiosInstance.interceptors.response.use(
 		// Do something with response error
 
 		if (!error.response) {
-			toast.error('Network Error');
 			console.log(error);
+			switch (error.status) {
+				case 401:
+					toast.error('Unauthorized Redirecting to login page in 3 seconds');
+					setTimeout(clearEverything, 3000);
+					break;
+				case 400:
+					toast.error('Bad Request');
+					break;
+				case 404:
+					toast.error('Not Found');
+					break;
+				case 500:
+					toast.error('Server Error');
+					break;
+				default:
+					toast.error('An error occurred');
+			}
 
 			return Promise.reject(error);
 		}
@@ -32,8 +60,10 @@ axiosInstance.interceptors.response.use(
 
 		switch (status) {
 			case 401:
-				toast.error('Unauthorized Redirecting to login page');
-				redirect('/login');
+				toast.error('Unauthorized Redirecting to login page in 3 Seconds');
+
+				setTimeout(clearEverything, 3000);
+
 				break;
 			case 400:
 				toast.error(data.message || 'Bad Request');
