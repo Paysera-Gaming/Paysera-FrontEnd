ebe8af899aac211d33d5add9db4a23bf169aeb03
luekely <luekely43110@gmail.com>
2025-03-14 21:07:39 +0800

    added the epic forms

--- src/components/TimeInComponent/RequestLeaveButton.tsx
+++ src/components/TimeInComponent/RequestLeaveButton.tsx
@@ -1,4 +1,3 @@
-import { requestOvertime } from '@/api/OvertimeAPI';
 import { Button } from '@/components/ui/button';
 import {
 	Dialog,
@@ -9,6 +8,27 @@ import {
 	DialogTitle,
 	DialogTrigger,
 } from '@/components/ui/dialog';
+
+import {
+	Popover,
+	PopoverContent,
+	PopoverTrigger,
+} from '@/components/ui/popover';
+
+import { format } from 'date-fns';
+import { CalendarIcon } from 'lucide-react';
+
+import { Calendar } from '@/components/ui/calendar';
+
+import {
+	Form,
+	FormControl,
+	FormDescription,
+	FormField,
+	FormItem,
+	FormLabel,
+	FormMessage,
+} from '@/components/ui/form';
 import { Label } from '@/components/ui/label';
 
 import {
@@ -20,32 +40,49 @@ import {
 	SelectTrigger,
 	SelectValue,
 } from '@/components/ui/select';
+
+import { zodResolver } from '@hookform/resolvers/zod';
+import { useForm } from 'react-hook-form';
+import { z } from 'zod';
+
 import { useUserStore } from '@/stores/userStore.ts';
 import { cn } from '@/lib/utils';
 import { useMutation } from '@tanstack/react-query';
-import { Loader2 } from 'lucide-react';
+import { Info, Loader2 } from 'lucide-react';
 import { useState } from 'react';
 import { toast } from 'sonner';
+import {
+	Tooltip,
+	TooltipContent,
+	TooltipProvider,
+	TooltipTrigger,
+} from '../ui/tooltip';
+import { Textarea } from '../ui/textarea';
 
-// get
-// get attendance today
-// get attendance that requests overtime
-// team lead can edit the requested overtime hours
-// after that approve or reject`
-// teamlead can descerne if the request is rejected or not
+// this should insert what type of leave this is
+// this should get the start and end of the date of this leave
+// this should get the reasons of leaving
+
+const formSchema = z.object({
+	startDate: z.date(),
+	endDate: z.date(),
+	reason: z.string(),
+	type: z.string(),
+});
 
 export function RequestLeaveButton() {
-	const [getHours, setHours] = useState<number>(0);
-
 	const [isOpen, setIsOpen] = useState<boolean>(false);
 	const [isLoading, setIsLoading] = useState<boolean>(false);
+
 	function handleSubmit() {
 		setIsLoading(true);
 		mutation.mutate();
 	}
 
 	const employeeId = useUserStore.getState().user?.id;
-
+	const form = useForm<z.infer<typeof formSchema>>({
+		resolver: zodResolver(formSchema),
+	});
 	const mutation = useMutation({
 		mutationFn: () => {
 			console.log(employeeId);
@@ -53,21 +90,19 @@ export function RequestLeaveButton() {
 			if (employeeId === undefined) {
 				throw new Error('Employee ID is undefined');
 			}
-			return requestOvertime({
-				employeeId: employeeId,
-				timeStamp: new Date(),
-				limitOvertime: getHours,
-			});
+			return;
 		},
 		onSuccess: () => {
-			toast.success('Request for overtime has successfully submitted');
+			toast.success('Request for Leave has successfully submitted');
 		},
 		onSettled: () => {
 			setIsLoading(false);
 			setIsOpen(false);
 		},
 	});
-
+	function onSubmit() {
+		console.log(' I am now being mutated');
+	}
 	return (
 		<Dialog open={isOpen} onOpenChange={setIsOpen}>
 			<DialogTrigger asChild>
@@ -77,46 +112,162 @@ export function RequestLeaveButton() {
 			</DialogTrigger>
 			<DialogContent className="sm:max-w-[425px]">
 				<DialogHeader>
-					<DialogTitle>Requesting of overtime form</DialogTitle>
+					<DialogTitle>Requesting Leave form</DialogTitle>
 					<DialogDescription>
-						Select the amount of hours to render for your request. Click submit
-						when you're done.
+						You are about to create an Leave Form. Make sure to fill out all the
+						necessary information accurately.
 					</DialogDescription>
 				</DialogHeader>
+				<Form {...form}>
+					<form onSubmit={form.handleSubmit(onSubmit)}>
+						<FormField
+							control={form.control}
+							name="type"
+							render={({ field }) => (
+								<FormItem>
+									<div className="flex items-center justify-start gap-2">
+										<FormLabel>Leave Type</FormLabel>
+										<TooltipProvider>
+											<Tooltip>
+												<TooltipTrigger asChild>
+													<Info className="w-4 h-4"></Info>
+												</TooltipTrigger>
+												<TooltipContent>
+													This dictates the type of leave you'll be sending
+												</TooltipContent>
+											</Tooltip>
+										</TooltipProvider>
+									</div>
+									<Select
+										onValueChange={field.onChange}
+										defaultValue={field.value}
+									>
+										<FormControl>
+											<SelectTrigger>
+												<SelectValue placeholder="Select a Schedule Type" />
+											</SelectTrigger>
+										</FormControl>
+										<SelectContent>
+											<SelectItem value="Medical_Leave">
+												Medical Leave
+											</SelectItem>
+											<SelectItem value="Maternal_Leave">
+												Maternal Leave
+											</SelectItem>
+											<SelectItem value="Sick_Leave">Sick Leave</SelectItem>
+											<SelectItem value="Paternity_Leave">
+												Paternity Leave
+											</SelectItem>
+										</SelectContent>
+									</Select>
+									<FormMessage />
+								</FormItem>
+							)}
+						/>
 
-				<Label htmlFor="name" className="">
-					How many hours to render
-				</Label>
+						<FormField
+							control={form.control}
+							name="startDate"
+							render={({ field }) => (
+								<FormItem>
+									<FormLabel>Leave Starts At:</FormLabel>
+									<FormControl>
+										{' '}
+										<Popover>
+											<PopoverTrigger asChild>
+												<Button
+													variant={'outline'}
+													className={cn(
+														'w-[240px] justify-start text-left font-normal',
+														!field.value && 'text-muted-foreground'
+													)}
+												>
+													<CalendarIcon />
+													{field.value ? (
+														format(field.value, 'PPP')
+													) : (
+														<span>Pick a date</span>
+													)}
+												</Button>
+											</PopoverTrigger>
+											<PopoverContent className="w-auto p-0" align="start">
+												<Calendar
+													mode="single"
+													selected={field.value}
+													onSelect={field.onChange}
+													initialFocus
+												/>
+											</PopoverContent>
+										</Popover>
+									</FormControl>
+								</FormItem>
+							)}
+						/>
 
+						<FormField
+							control={form.control}
+							name="endDate"
+							render={({ field }) => (
+								<FormItem>
+									<FormLabel>Leave Ends At:</FormLabel>
+									<FormControl>
+										{' '}
+										<Popover>
+											<PopoverTrigger asChild>
+												<Button
+													variant={'outline'}
+													className={cn(
+														'w-[240px] justify-start text-left font-normal',
+														!field.value && 'text-muted-foreground'
+													)}
+												>
+													<CalendarIcon />
+													{field.value ? (
+														format(field.value, 'PPP')
+													) : (
+														<span>Pick a date</span>
+													)}
+												</Button>
+											</PopoverTrigger>
+											<PopoverContent className="w-auto p-0" align="start">
+												<Calendar
+													mode="single"
+													selected={field.value}
+													onSelect={field.onChange}
+													initialFocus
+												/>
+											</PopoverContent>
+										</Popover>
+									</FormControl>
+								</FormItem>
+							)}
+						/>
+
+						<FormField
+							control={form.control}
+							name="reason"
+							render={({ field }) => (
+								<FormItem>
+									<FormLabel>Bio</FormLabel>
+									<FormControl>
+										<Textarea
+											placeholder="Tell us a little bit about yourself"
+											className="resize-none"
+											{...field}
+										/>
+									</FormControl>
+									<FormDescription>
+										Optional: Add reason for the leave
+									</FormDescription>
+									<FormMessage />
+								</FormItem>
+							)}
+						/>
+					</form>
+				</Form>
 				<div className="flex gap-2">
-					<Select onValueChange={(value) => setHours(Number(value))}>
-						<SelectTrigger
-							className="w-[280export function RequestLeaveButton() {
-px]"
-						>
-							<SelectValue placeholder="Select hours to render on overtime" />
-						</SelectTrigger>
-						<SelectContent>
-							<SelectGroup>
-								<SelectLabel>Available hours</SelectLabel>
-								<SelectItem value="1">1</SelectItem>
-								<SelectItem value="2">2</SelectItem>
-								<SelectItem value="3">3</SelectItem>
-								<SelectItem value="4">4</SelectItem>
-								<SelectItem value="5">5</SelectItem>
-								<SelectItem value="6">6</SelectItem>
-								<SelectItem value="7">7</SelectItem>
-								<SelectItem value="8">8</SelectItem>
-							</SelectGroup>
-						</SelectContent>
-					</Select>
-
 					<DialogFooter>
-						<Button
-							onClick={handleSubmit}
-							disabled={getHours == 0 || isLoading}
-							type="submit"
-						>
+						<Button onClick={handleSubmit} disabled={isLoading} type="submit">
 							<Loader2
 								className={cn(
 									isLoading == false && 'hidden',
